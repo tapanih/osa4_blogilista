60e4ea7303ce721b0c530f6a20045a01436a1b2a
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-06 16:11:45 +0300

    Siirretty sovellus tiedostoon app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(config.MONGODB_URI, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (req, res) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog
+    .save()
+    .then(result => {
+      res.status(201).json(result)
+    })
+})
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,44 +1,9 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI, { 
-  useNewUrlParser: true,
-  useUnifiedTopology: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
